7b2631cbc5901303fcac8889d4f3015759faa818
Ole <refsa22@gmail.com>
2021-07-29 20:11:19 +0200

    refactor: cleaned up GlobalEventBus

--- EventBus/GlobalEventBus.cs
+++ EventBus/GlobalEventBus.cs
@@ -2,25 +2,13 @@ using System.Collections.Generic;
 
 namespace Refsa.EventBus
 {
-    public class GlobalEventBus
+    public static class GlobalEventBus
     {
-        static GlobalEventBus instance;
-        public static GlobalEventBus Instance => instance;
-
-        EventBus eventBus;
-        public static Stack<BusLock> IncomingMessages => instance.eventBus.IncomingMessages;
-
-        public static EventBus Bus => instance.eventBus;
+        public static readonly EventBus EventBus;
 
         static GlobalEventBus()
         {
-            if (instance == null) instance = new GlobalEventBus();
-            else return;
-        }
-
-        GlobalEventBus()
-        {
-            eventBus = new EventBus();
+            EventBus = new EventBus();
         }
     }
 }
