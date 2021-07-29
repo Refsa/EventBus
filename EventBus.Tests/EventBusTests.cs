3a818685556264638940a0667cbf90dac555c47b
Ole <refsa22@gmail.com>
2021-07-29 22:02:31 +0200

    test: case for multiple busses test: case for publish from thread

--- EventBus.Tests/EventBusTests.cs
+++ EventBus.Tests/EventBusTests.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Linq;
 using Xunit;
 using Refsa.EventBus;
 
@@ -14,17 +15,90 @@ namespace Refsa.EventBus.Tests
         [Fact]
         public void sub_and_pub_message()
         {
-            var msg = new TestMessage{Content = 1234};
+            var msg = new TestMessage { Content = 1234 };
             var bus = new EventBus();
 
             int received = 0;
-            bus.Sub<TestMessage>((in TestMessage m) => {
+            bus.Sub<TestMessage>((in TestMessage m) =>
+            {
                 received = m.Content;
             });
 
             bus.Pub(msg);
 
             Assert.Equal(msg.Content, received);
+        }
+
+        [Fact]
+        public void busses_dont_overlap()
+        {
+            var msg1 = new TestMessage { Content = 1234 };
+            var msg2 = new TestMessage { Content = 4321 };
+
+            var eb1 = new EventBus();
+            var eb2 = new EventBus();
+
+            int recv1 = 0;
+            eb1.Sub<TestMessage>((in TestMessage m) => { recv1 = m.Content; });
+            int recv2 = 0;
+            eb2.Sub<TestMessage>((in TestMessage m) => { recv2 = m.Content; });
+
+            eb1.Pub(msg1);
+            eb2.Pub(msg2);
+
+            Assert.Equal(msg1.Content, recv1);
+            Assert.Equal(msg2.Content, recv2);
+        }
+
+        [Fact]
+        public void event_bus_can_be_used_in_multiple_threads()
+        {
+            var msg1 = new TestMessage { Content = 1234 };
+            var eb = new EventBus();
+
+            var recv = new System.Collections.Concurrent.ConcurrentBag<int>();
+            eb.Sub<TestMessage>((in TestMessage m) => { recv.Add(m.Content); });
+
+            int taskCount = 100;
+
+            var startTime = DateTime.Now + TimeSpan.FromSeconds(2);
+            var tasks = new System.Collections.Generic.List<System.Threading.Tasks.Task>();
+            for (int i = 0; i < taskCount; i++)
+            {
+                int j = i;
+                tasks.Add(System.Threading.Tasks.Task.Run(() =>
+                {
+                    while (DateTime.Now < startTime)
+                    {
+                        System.Threading.Thread.Sleep(1);
+                    }
+
+                    eb.Pub(new TestMessage { Content = j });
+                }));
+            }
+
+            System.Threading.Tasks.Task.WaitAll(tasks.ToArray());
+
+            var expected = new System.Collections.Generic.HashSet<int>();
+
+            Assert.Equal(taskCount, recv.Count);
+            while(recv.TryTake(out int value))
+            {
+                expected.Add(value);
+            }
+
+            Assert.Equal(taskCount, expected.Count);
+            Assert.Equal(CalcSum(taskCount), expected.Sum());
+        }
+
+        int CalcSum(int to)
+        {
+            int sum = 0;
+            for (int i = 0; i < to; i++)
+            {
+                sum += i;
+            }
+            return sum;
         }
     }
 }
