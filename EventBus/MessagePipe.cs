e0cd0eecbb6bb42f6f4ae79c27389bb21c3d5be2
Refsa <refsa22@gmail.com>
2022-01-18 11:40:24 +0100

    feat: more helper functions on MessagePipe chore: xml comments on MessagePipe

--- EventBus/MessagePipe.cs
+++ EventBus/MessagePipe.cs
@@ -17,16 +17,65 @@ namespace Refsa.EventBus
             bus = new MessageBus();
         }
 
+        /// <summary>
+        /// Sets the parent pipe
+        /// </summary>
+        public void SetParent(MessagePipe pipe)
+        {
+            this.parent = pipe;
+        }
+
+        /// <summary>
+        /// Adds a child pipe
+        /// </summary>
         public void AddChild(MessagePipe messagePipe)
         {
             children.Add(messagePipe);
         }
 
+        /// <summary>
+        /// Removes a child pipe
+        /// </summary>
+        public void RemoveChild(MessagePipe pipe)
+        {
+            children.Remove(pipe);
+        }
+
+        /// <summary>
+        /// Sub to messages on this pipe
+        /// </summary>
+        /// <param name="callback"></param>
+        /// <typeparam name="TMessage"></typeparam>
         public void Sub<TMessage>(MessageHandlerDelegates.MessageHandler<TMessage> callback) where TMessage : IMessage
         {
             bus.Sub(callback);
         }
 
+        /// <summary>
+        /// Unsub from a message on this pipe
+        /// </summary>
+        /// <param name="callback"></param>
+        /// <typeparam name="TMessage"></typeparam>
+        public void UnSub<TMessage>(MessageHandlerDelegates.MessageHandler<TMessage> callback) where TMessage : IMessage
+        {
+            bus.UnSub<TMessage>(callback);
+        }
+
+        /// <summary>
+        /// Pub message to this message pipe
+        /// </summary>
+        /// <param name="message"></param>
+        /// <typeparam name="TMessage"></typeparam>
+        public void Pub<TMessage>(in TMessage message) where TMessage : IMessage
+        {
+            bus.Pub(message);
+        }
+
+        /// <summary>
+        /// Pub message to this pipe and the hierarchy below it
+        /// </summary>
+        /// <param name="message"></param>
+        /// <typeparam name="TMessage"></typeparam>
         public void PubDown<TMessage>(in TMessage message) where TMessage : IMessage
         {
             bus.Pub(message);
@@ -36,17 +85,33 @@ namespace Refsa.EventBus
             }
         }
 
+        /// <summary>
+        /// Pub message to this pipe and the hierarchy above it
+        /// </summary>
+        /// <param name="message"></param>
+        /// <typeparam name="TMessage"></typeparam>
         public void PubUp<TMessage>(in TMessage message) where TMessage : IMessage
         {
             bus.Pub(message);
             parent?.PubUp(message);
         }
 
+        /// <summary>
+        /// Pub message to this node hierarchy and its sibling hierarchies
+        /// </summary>
+        /// <param name="message"></param>
+        /// <typeparam name="TMessage"></typeparam>
         public void PubSide<TMessage>(in TMessage message) where TMessage : IMessage
         {
             parent?.PubDown(message, this);
         }
 
+        /// <summary>
+        /// Pub a message to the child hierarchy, ignoring the given pipe
+        /// </summary>
+        /// <param name="message"></param>
+        /// <param name="exclude"></param>
+        /// <typeparam name="TMessage"></typeparam>
         void PubDown<TMessage>(in TMessage message, MessagePipe exclude) where TMessage : IMessage
         {
             foreach (var child in children)
@@ -54,11 +119,6 @@ namespace Refsa.EventBus
                 if (child == exclude) continue;
                 child.PubDown(message);
             }
-        }
-
-        public void UnSub<TMessage>(MessageHandlerDelegates.MessageHandler<TMessage> callback) where TMessage : IMessage
-        {
-            bus.UnSub<TMessage>(callback);
         }
     }
 }
