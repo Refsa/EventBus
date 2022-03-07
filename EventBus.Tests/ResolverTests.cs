c3866557bff4e688c8bb59425fab9401d6ebb75a
Refsa <refsa22@gmail.com>
2022-03-07 17:59:19 +0100

    test: case for adding to SparseSetResolver

---
+++ EventBus.Tests/ResolverTests.cs
@@ -0,0 +1,36 @@
+using System;
+using System.Linq;
+using Xunit;
+using Refsa.EventBus;
+using System.Reflection;
+
+namespace Refsa.EventBus.Tests
+{
+    public class ResolverTests
+    {
+        struct TestMessage1 : IMessage { }
+        struct TestMessage2 : IMessage { }
+
+        static readonly FieldInfo sparseSetFI = typeof(SparseSetResolver).GetField("indices", BindingFlags.Instance | BindingFlags.NonPublic);
+        static readonly FieldInfo sparseSetCountFI = typeof(SparseSetResolver).GetNestedType("SparseSet", BindingFlags.NonPublic).GetField("count", BindingFlags.NonPublic | BindingFlags.Instance);
+
+        [Fact]
+        public void SparseSetResolver_Add()
+        {
+            var resolver = new SparseSetResolver();
+            var sparseSet = sparseSetFI.GetValue(resolver);
+            int count = 0;
+
+            for (int i = 0; i < 100; i++)
+            {
+                resolver.GetHandler<TestMessage1>();
+                count = (int)sparseSetCountFI.GetValue(sparseSet);
+                Assert.Equal(1, count);
+            }
+
+            resolver.GetHandler<TestMessage2>();
+            count = (int)sparseSetCountFI.GetValue(sparseSet);
+            Assert.Equal(2, count);
+        }
+    }
+}
