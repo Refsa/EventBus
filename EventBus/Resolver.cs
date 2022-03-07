1d7073e3ae51185db395a42762ab32ccda963785
Refsa <refsa22@gmail.com>
2022-03-07 19:33:46 +0100

    feat: added a StaticResolver as a future replacement for SparseSetResolver

--- EventBus/Resolver.cs
+++ EventBus/Resolver.cs
@@ -46,109 +46,6 @@ namespace Refsa.EventBus
     public class SparseSetResolver : IResolver
     {
         /// <summary>
-        /// A very simple sparse set implementation
-        /// </summary>
-        class SparseSet
-        {
-            int max;
-            int count;
-            int[] dense;
-            int[] sparse;
-
-            public SparseSet(int max)
-            {
-                this.max = max;
-                this.count = 0;
-                this.sparse = new int[max];
-                this.dense = new int[max];
-            }
-
-            /// <summary>
-            /// Adds a value to the set
-            /// </summary>
-            /// <param name="value"></param>
-            /// <returns>The index of the value into the dense set</returns>
-            public int Add(int value)
-            {
-                if (value >= max)
-                {
-                    Grow();
-                }
-
-                if (value >= 0 && !Contains(value))
-                {
-                    dense[count] = value;
-                    sparse[value] = count;
-                    count++;
-                    return count - 1;
-                }
-
-                return Get(value);
-            }
-
-            /// <summary>
-            /// Get the index into the dense set of the value
-            /// </summary>
-            /// <param name="value"></param>
-            /// <returns>dense set index or -1 if it's not in the set</returns>
-            public int Get(int value)
-            {
-                if (value >= 0 && value < max && Contains(value))
-                {
-                    return sparse[value];
-                }
-
-                return -1;
-            }
-
-            /// <summary>
-            /// Removes a value from the set
-            /// </summary>
-            /// <param name="value"></param>
-            public void Remove(int value)
-            {
-                if (Contains(value))
-                {
-                    dense[sparse[value]] = dense[count - 1];
-                    sparse[dense[count - 1]] = sparse[value];
-                    count--;
-                }
-            }
-
-            /// <summary>
-            /// Checks if value is in the set
-            /// </summary>
-            /// <param name="value"></param>
-            /// <returns></returns>
-            public bool Contains(int value)
-            {
-                if (value >= max || value < 0) return false;
-
-                return sparse[value] < count && dense[sparse[value]] == value;
-            }
-
-            /// <summary>
-            /// Resets internal index counter, does not clear the contents
-            /// </summary>
-            public void Clear()
-            {
-                count = 0;
-            }
-
-            void Grow()
-            {
-                max = max * 2 + 1;
-                var new_d = new int[max];
-                var new_s = new int[max];
-                dense.CopyTo(new_d, 0);
-                sparse.CopyTo(new_s, 0);
-
-                dense = new_d;
-                sparse = new_s;
-            }
-        }
-
-        /// <summary>
         /// the global message type counter
         /// </summary>
         static volatile int messageTypeCounter;
@@ -167,7 +64,7 @@ namespace Refsa.EventBus
 
         public SparseSetResolver()
         {
-            indices = new SparseSet(10);
+            indices = new SparseSet(0);
             handlers = new List<IHandler<IMessage>>();
         }
 
@@ -185,6 +82,78 @@ namespace Refsa.EventBus
     }
 
     /// <summary>
+    /// 
+    /// </summary>
+    public class StaticResolver : IResolver
+    {
+        static class HandlerResolver<TMessage> where TMessage : IMessage
+        {
+            static List<IHandler<IMessage>> handlers = new List<IHandler<IMessage>>();
+
+            public static IHandler<IMessage> Get(int index, out bool created)
+            {
+                created = false;
+                while (index >= handlers.Count)
+                {
+                    handlers.Add(null);
+                }
+                if (handlers[index] == null)
+                {
+                    handlers[index] = new MessageHandler<TMessage>();
+                    created = true;
+                }
+
+                return handlers[index];
+            }
+
+            public static void Clear(int index)
+            {
+                if (index >= handlers.Count) return;
+                handlers[index] = null;
+            }
+        }
+
+        static readonly SparseSet resolverIndices;
+        static int resolverCounter;
+        static readonly List<System.Action<int>> resolverClears;
+        static StaticResolver()
+        {
+            resolverIndices = new SparseSet(0);
+            resolverClears = new List<System.Action<int>>();
+        }
+
+        ~StaticResolver()
+        {
+            resolverIndices.Remove(resolverCount);
+            foreach (var clear in resolverClears)
+            {
+                clear.Invoke(resolverIndex);
+            }
+        }
+
+        int resolverIndex;
+        int resolverCount;
+
+        public StaticResolver()
+        {
+            resolverCount = resolverCounter++;
+            resolverIndex = resolverIndices.Add(resolverCount);
+        }
+
+        public MessageHandler<TMessage> GetHandler<TMessage>() where TMessage : IMessage
+        {
+            var handler = (MessageHandler<TMessage>)HandlerResolver<TMessage>.Get(resolverIndex, out bool created);
+
+            if (created)
+            {
+                resolverClears.Add(HandlerResolver<TMessage>.Clear);
+            }
+
+            return handler;
+        }
+    }
+
+    /// <summary>
     /// Resolver for storing handlers in the global scope <br/>
     /// 
     /// All handlers are shared between any instance of GlobalResolver
@@ -194,12 +163,115 @@ namespace Refsa.EventBus
         static class Resolver<TMessage> where TMessage : IMessage
         {
             static readonly MessageHandler<TMessage> handler = new MessageHandler<TMessage>();
-            public static MessageHandler<TMessage> Handler => handler;   
+            public static MessageHandler<TMessage> Handler => handler;
         }
 
         public MessageHandler<TMessage> GetHandler<TMessage>() where TMessage : IMessage
         {
             return Resolver<TMessage>.Handler;
+        }
+    }
+
+    /// <summary>
+    /// A very simple sparse set implementation
+    /// </summary>
+    public class SparseSet
+    {
+        int max;
+        int count;
+        int[] dense;
+        int[] sparse;
+
+        public SparseSet(int max)
+        {
+            this.max = max;
+            this.count = 0;
+            this.sparse = new int[max];
+            this.dense = new int[max];
+        }
+
+        /// <summary>
+        /// Adds a value to the set
+        /// </summary>
+        /// <param name="value"></param>
+        /// <returns>The index of the value into the dense set</returns>
+        public int Add(int value)
+        {
+            if (value >= max)
+            {
+                Grow();
+            }
+
+            if (value >= 0 && !Contains(value))
+            {
+                dense[count] = value;
+                sparse[value] = count;
+                count++;
+                return count - 1;
+            }
+
+            return Get(value);
+        }
+
+        /// <summary>
+        /// Get the index into the dense set of the value
+        /// </summary>
+        /// <param name="value"></param>
+        /// <returns>dense set index or -1 if it's not in the set</returns>
+        public int Get(int value)
+        {
+            if (value >= 0 && value < max && Contains(value))
+            {
+                return sparse[value];
+            }
+
+            return -1;
+        }
+
+        /// <summary>
+        /// Removes a value from the set
+        /// </summary>
+        /// <param name="value"></param>
+        public void Remove(int value)
+        {
+            if (Contains(value))
+            {
+                dense[sparse[value]] = dense[count - 1];
+                sparse[dense[count - 1]] = sparse[value];
+                count--;
+            }
+        }
+
+        /// <summary>
+        /// Checks if value is in the set
+        /// </summary>
+        /// <param name="value"></param>
+        /// <returns></returns>
+        public bool Contains(int value)
+        {
+            if (value >= max || value < 0) return false;
+
+            return sparse[value] < count && dense[sparse[value]] == value;
+        }
+
+        /// <summary>
+        /// Resets internal index counter, does not clear the contents
+        /// </summary>
+        public void Clear()
+        {
+            count = 0;
+        }
+
+        void Grow()
+        {
+            max = (max + 1) * 2 + 1;
+            var new_d = new int[max];
+            var new_s = new int[max];
+            dense.CopyTo(new_d, 0);
+            sparse.CopyTo(new_s, 0);
+
+            dense = new_d;
+            sparse = new_s;
         }
     }
 }
